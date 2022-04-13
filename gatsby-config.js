6192d2ddc48a9da63b6dc470d31ebd10537fcf41
soft <obisopulu@gmail.com>
2022-04-13 15:48:45 +0200

    add linting

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,18 @@
  * https://www.gatsbyjs.com/docs/gatsby-config/
  *
  */
-
+ const path = require("path");
+ // Get paths of Gatsby's required rules, which as of writing is located at:
+ // https://github.com/gatsbyjs/gatsby/tree/fbfe3f63dec23d279a27b54b4057dd611dce74bb/packages/
+ // gatsby/src/utils/eslint-rules
+ const gatsbyRequiredRules = path.join(
+   process.cwd(),
+   "node_modules",
+   "gatsby",
+   "dist",
+   "utils",
+   "eslint-rules"
+ );
 module.exports = {
   /**
    * Adding plugins to this array adds them to your Gatsby site.
@@ -28,7 +39,20 @@ module.exports = {
         // the only required plugin option for WordPress is the GraphQL url.
         url:
           process.env.WPGRAPHQL_URL ||
-          `https://wpgatsbydemo.wpengine.com/graphql`,
+          `https://gatsby.welitzki-it.de/graphql`,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-eslint",
+      options: {
+        // Gatsby required rules directory
+        rulePaths: [gatsbyRequiredRules],
+        // Default settings that may be ommitted or customized
+        stages: ["develop"],
+        extensions: ["js", "jsx", "ts", "tsx"],
+        exclude: ["node_modules", "bower_components", ".cache", "public"],
+        // Any additional eslint-webpack-plugin options below
+        // ...
       },
     },
 
